129ccb19cdb69fa11cccaf1d88506763b52080f2
Satnam-Gill <ersatnamgill@gmail.com>
2024-11-29 10:19:59 +0530

    (Add: Aaronsburg Abbottstown Abington Acme Adamsburg Adamstown Adrian Airville Akron Albrightsville Alburtis Aleppo Alexandria Allenport Allensville Allenwood Alverton Amberson Amity Andreas Annville Apollo Aquashicola Ardara Arendtsville Aristes Armbrust Arona Ashfield Ashland Aspers Aston Atglen Atlasburg Auburn Avella Avis Avondale Avonmore Baden Bainbridge Bairdford Bakerstown Bally Barnesville Barto Bartonsville Bath Bausman Beallsville Bear Creek Beaver Meadows Beaver Beaver Springs Beavertown Bechtelsville Beech Creek Belle Vernon Belleville Bendersville Bensalem Bentleyville Benton Bernville Berrysburg Berwyn Bethel Biglerville Bird In Hand Blain Blairs Mills Blairsville Blakeslee Blanchard Blue Ball Blue Ridge Summit Boalsburg Bobtown Boiling Springs Bolivar Bovard Bowers Bowmanstown Bowmansville Boyers Boyertown Brackenridge Braddock Bradenville Bradfordwoods Branchdale Brandamore Brave Bristol Broad Top Brockton Brodheadsville Brogue Brownstown Bruin Bryn Athyn Buck Hill Falls Buckingham Buena Vista Bulger Bunola Burgettstown Burnham Bushkill Cabot Cadogan Callery Calvin Cammal Canadensis Carmichaels Carversville Cassville Catawissa Cecil Cedar Run Center Valley Centerport Centre Hall Chadds Ford Chalfont Charleroi Cheltenham Cherryville Chester Heights Chester Springs Cheswick Cheyney Chicora Christiana Clarence Claridge Clark Clarksburg Clarksville Claysville Clinton Coal Center Coaldale Coburn Cochranville Codorus Cogan Station Cokeburg Colmar Concord Conestoga Connoquenessing Conway Conyngham Coopersburg Coplay Coulters Cowansville Crabtree Cranberry Township Creighton Crescent Cresco Cressona Cross Fork Crucible Crum Lynne Cuddy Cumbola)

--- app/components/Widgets/CounterCta.tsx
+++ app/components/Widgets/CounterCta.tsx
@@ -7,36 +7,34 @@ import { FaBuildingCircleCheck } from "react-icons/fa6";
 import { RiTeamFill } from "react-icons/ri";
 import { VscPreview } from "react-icons/vsc";
 import { FaAward } from "react-icons/fa";
+import Image from "next/image";
 
 const CounterCta = () => {
   return (
-    <div className="mt-20 bg-main text-white">
-      <div className="item-center flex flex-col justify-around border px-10 py-10 md:flex-row md:py-20">
-        <div className="md:w-[45%]">
+    <div className="mt-20  text-white relative">
+      <div className="">
+
+      </div>
+      <div className="item-center flex flex-col justify-around items-center px-4 md:px-10 py-10 md:flex-row md:py-10 relative bg-black/70 z-20">
+        <div className="max-w-4xl mx-auto text-center relative ">
           {/* <div className="w-fit rounded-full border border-white p-1 px-2 text-sm  font-semibold text-minor">
             NUMBER TALKS
           </div> */}
-          <div className="mt-2 text-5xl">
-          For Fast, Affordable <span className="text-minor">Roofing,</span> Call the Team You Can Trust!
+          <div className="mt-2 text-4xl">
+          Get a Free <span className="text-main">Roof</span> Estimate Today! 
           </div>
           <div className="mt-2">
           When you need roofing help, don’t settle for delays or uncertainty—call Affordable Roofing now for fast, dependable service you can count on! Our experienced team is ready to handle any roofing issue, big or small, with precision and care.
           </div>
-          <div className="mt-4 hidden gap-4 md:flex">
-            <a href={`tel:${ContactInfo.tel}`}>
-              <p className="flex  h-14 w-14 items-center justify-center rounded-full bg-minor text-2xl duration-150 ease-in-out hover:scale-[1.2]">
-                <IoCallSharp />
-              </p>
-            </a>
-            <div className="flex flex-col text-lg ">
-              <div className="">Need Help?</div>
-              <div className=" font-bold">
+          <div className="mt-4  gap-4 flex justify-center ">
+            <div className="flex flex-col text-lg  border px-4 py-2 bg-white rounded-md">
+              <div className=" font-bold text-main">
                 <a href={`tel:${ContactInfo.tel}`}>{ContactInfo.No}</a>
               </div>
             </div>
           </div>
         </div>
-        <div className="mdjustify-center mt-4 grid  grid-cols-2 gap-4 md:mt-0 md:w-[40%]">
+        {/* <div className="mdjustify-center mt-4 grid  grid-cols-2 gap-4 md:mt-0 md:w-[40%]">
           <div className="flex flex-col items-center justify-center rounded-2xl border p-4 px-4 md:w-60 md:p-0 ">
             <div className="flex flex-col items-center  justify-between gap-4  font-bold md:flex-row ">
               <FaBuildingCircleCheck className="text-6xl text-minor" />
@@ -65,21 +63,17 @@ const CounterCta = () => {
             </div>
             <div className="mt-2 text-center">Our Winning Award</div>
           </div>
-        </div>
-        <div className="mt-10 flex gap-4 md:hidden">
-          <a href={`tel:${ContactInfo.tel}`}>
-            <p className="flex  h-14 w-14 items-center justify-center rounded-full bg-minor text-2xl duration-150 ease-in-out hover:scale-[1.2]">
-              <IoCallSharp />
-            </p>
-          </a>
-          <div className="flex flex-col text-lg ">
-            <div className="">Need Help?</div>
-            <div className=" font-bold">
-              <a href={`tel:${ContactInfo.tel}`}>{ContactInfo.No}</a>
-            </div>
-          </div>
-        </div>
+        </div> */}
       </div>
+   <div className="absolute bottom-0 left-0 top-0  right-0 ">
+    <Image
+      height={1000}
+      width={1000}
+      className="w-full h-full object-cover object-center "
+      src="/maintenance-worker-is-diligently-repairing-leaky-roof-residential-building-showcasing-skill-focus-scene-captures-essence-hard-work-dedication-home-maintenance.jpg"
+      alt="counter-cta"
+    />
+   </div>
     </div>
   );
 };
