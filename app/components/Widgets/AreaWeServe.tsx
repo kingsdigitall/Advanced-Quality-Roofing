635f83679dfd6485fa743fa236c237cf2d274a12
Satnam-Gill <ersatnamgill@gmail.com>
2025-01-07 13:05:37 +0530

    (Add: Show Less button)

--- app/components/Widgets/AreaWeServe.tsx
+++ app/components/Widgets/AreaWeServe.tsx
@@ -5,14 +5,18 @@ import ContactInfo from "@/components/Content/ContactInfo.json";
 
 const AreaWeServe = ({ slugs }: any) => {
   const [showAll, setShowAll] = useState(false);
-  const initialCount = 40;
+  const initialCount = 30;
 
   const handleReadMore = () => {
     setShowAll(true);
   };
 
+  const handleShowLess = () => {
+    setShowAll(false);
+  };
+
   return (
-    <div className="mx-6 md:mx-10 mt-10 flex h-fit w-auto flex-wrap gap-4 justify-center items-center">
+    <div  className="mx-6 md:mx-10 flex h-fit w-auto flex-wrap gap-4 mt-4 justify-center items-center scroll-smooth">
       {slugs
         .sort()
         .slice(0, showAll ? slugs.length : initialCount)
@@ -39,6 +43,14 @@ const AreaWeServe = ({ slugs }: any) => {
           className="text-white bg-minor hover:bg-minor/90 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
         >
           View All
+        </button>
+      )}
+      {showAll && (
+        <button
+          onClick={handleShowLess}
+          className="text-white bg-minor hover:bg-minor/90 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+        >
+          <a href="#area-we-serve">Show Less</a>
         </button>
       )}
     </div>
